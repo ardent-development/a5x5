41173d5cab68a866698d30490dfececfa574a955
Akshat Singh <HackerDaGreat57@gmail.com>
2024-09-18 15:04:17 -0700

    Add my sync.sh

---
+++ sync.sh
@@ -0,0 +1,7 @@
+# sync.sh: unzips a file and replaces the current asset contents with stuff
+#          that's in the .zip (twisted_nematic57 uses this because she uses a
+#          Chromebook locked down heavily by school)
+
+rm -rfv 5x5-variable
+unzip 5x5-variable.zip
+rm -f 5x5-variable.zip
